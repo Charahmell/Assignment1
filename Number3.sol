bc7135bde4a28543923d8a6cb42a2fca93d01c55
Charahmell <159330708+Charahmell@users.noreply.github.com>
2026-01-13 15:23:16 +0100

    Add files via upload

---
+++ Number3.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.3;
+
+contract example {
+    struct product {
+        string product_name;
+        uint256 product_price;
+        uint256 product_quantity;
+        uint256 product_id;
+    }
+
+    mapping(address => userProfile) public user;
+    }
+
+    
